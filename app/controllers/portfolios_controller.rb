549e8df73d59bc56572c007f72956c7303d67e0c
Lukasz Bartminski <bartminskilukasz@gmail.com>
2018-10-15 21:59:54 +0200

    Added custom scopes for PORTFOLIOS

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,6 +3,10 @@ class PortfoliosController < ApplicationController
 		@portfolio_items = Portfolio.all
 	end
 
+  def show_ruby
+    @portfolio_items = Portfolio.show_only_ruby
+  end
+
 	def new
 		@portfolio_item = Portfolio.new
 	end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,11 @@
 class Portfolio < ApplicationRecord
 	validates_presence_of :title, :body, :main_image, :thumb_image
+
+	def self.show_only_angular
+		where(subtitle: 'Angular')
+	end
+
+	def self.show_only_ruby
+		where(subtitle: 'Ruby on Rails')
+	end
 end
