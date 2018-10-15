e4adb2423451cb0cc4879729888ef54a5279a5b2
Lukasz Bartminski <bartminskilukasz@gmail.com>
2018-10-15 22:22:33 +0200

    Added DEFAULT VALUES to the PORTFOLIO model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,7 +5,14 @@ class Portfolio < ApplicationRecord
 		where(subtitle: 'Angular')
 	end
 
-	def self.show_only_ruby
-		where(subtitle: 'Ruby on Rails')
+	scope :show_only_ruby, -> {where(subtitle: 'Ruby on Rails')}
+
+	#after_initialize tells app to use defaults values of attribute (portfolio, as we are in portfolio model) if none were provided during initilizing process
+	after_initialize :set_defaults
+
+	#set_defaults method tells app what defaults values should be; ||= -> this operand works like -> if side.left == nil, side.left == side.right
+	def set_defaults
+		self.main_image ||= "http://placehold.it/600x400"
+		self.thumb_image ||= "http://placehold.it/350x200"
 	end
 end
