c560a5757590c9bee138e2c71ee49dc8b502a5a6
Lukasz Bartminski <bartminskilukasz@gmail.com>
2018-10-16 16:14:38 +0200

    Added NESTED ATTRIBUTES for PORTFOLIO model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,10 @@
 class Portfolio < ApplicationRecord
-	#has_many says, Portfolio model may have many technologie, for example Ruby, RoR and HTML
+	#has_many says, Portfolio model may have many technologies, for example Ruby, RoR and HTML
 	has_many :technologies
+
+	#It simply accepts nested attributes, what means, I can create portfolio item in DB, and while creating this new item, I can create as many as I decide, techonologies attributes, also in DB, related to this new portfolio item. This is why we call it 'nested'.
+	accepts_nested_attributes_for :technologies, #reject_if rejects creation procces if no 'name' of nested attributes (at least one) is provided. But still, you can create Portfolio item without any nested attributes, at least at console.
+								   reject_if: lambda { |attrs| attrs['title'].blank? }
 
 	validates_presence_of :title, :body, :main_image, :thumb_image
 
