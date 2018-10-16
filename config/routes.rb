a36df4aa598d334995e65ae3be5a0f1bd1636986
Lukasz Bartminski <bartminskilukasz@gmail.com>
2018-10-17 00:28:48 +0200

    Added LOGIN, LOGOUT and REGISTRATION buttons and dynamic view rendering for them

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   #below, I have changed the routes for login, logout and registration; this syntax is specyfic for devise gem -> ,path: ''(...)
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show]
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
   	end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/rails/info/routes' => 'routes'
 
   root to: 'pages#home'
 end
