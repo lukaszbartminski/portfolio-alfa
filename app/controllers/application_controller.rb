9cdead8ca0f5474772b5dfd92a9f07e5d17886fd
Lukasz Bartminski <bartminskilukasz@gmail.com>
2018-10-18 23:35:17 +0200

    Repair push

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 class ApplicationController < ActionController::Base
 	include DeviseWhitelist
 	include SetSource
-
-	# nullObject pattern
-	def current_user
-		super || OpenStruct.new(name: "Guest User", first_name: "Guest", last_name: "User", email: "guest@example.com")
-	end
+	include CurrentUserConcern
 end
