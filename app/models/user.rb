1a46b314f1a044186e3598c1e42e4a8cdf1fa979
Lukasz Bartminski <bartminskilukasz@gmail.com>
2018-10-21 14:48:10 +0200

    Installed petergate gem and configured it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+  ############################################################################################
+  ## PeterGate Roles                                                                        ##
+  ## The :user role is added by default and shouldn't be included in this list.             ##
+  ## The :root_admin can access any page regardless of access settings. Use with caution!   ##
+  ## The multiple option can be set to true if you need users to have multiple roles.       ##
+  petergate(roles: [:admin, :editor], multiple: false)                                      ##
+  ############################################################################################ 
+ 
+
   # Include default devise modules. Others available are:
   # :confirmable (not active user until user confirm his email), :lockable(ability to lock user, if x login attempt failed), :timeoutable (logout user after x time) and :omniauthable (ability to log into app with Facebook, Gmail or so) :trackable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
