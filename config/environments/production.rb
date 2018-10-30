d079515a0e76344a544b3cafe754b8bed71f163f
Lukasz Bartminski <bartminskilukasz@gmail.com>
2018-10-30 22:05:40 +0100

    Modyfied production.rb to fix heroku error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-config.active_storage.serviceRails.application.configure do
+Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
